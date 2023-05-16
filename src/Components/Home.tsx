9e53f856a318de644994dd450b1325a92431c093
tanishaa17 <krati1701@gmail.com>
2023-05-16 17:57:21 +0530

    images added

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,7 +1,15 @@
+import  '../Images/Ellipse 704.jpg'
 export const Home = () => {
   return (
     <div className="home">
-      <div id="navDiv"></div>
+      <h1>Home page</h1>
+      <div id="navDiv">
+        <img
+          src="../Images/Ellipse 704.svg"
+          alt=""
+          style={{ border: "5px solid black", width: "100px" }}
+        />
+      </div>
       <div id="profileDiv"></div>
       <div id="headingDiv"></div>
       <div id="headersDiv"></div>
