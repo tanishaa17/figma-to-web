987f79b5587da3689f795e091bf84ddbb2c2013f
tanishaa17 <krati1701@gmail.com>
2023-05-16 17:08:41 +0530

    folder structure created

---
+++ src/Components/Home.tsx
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return (
+    <div className="home">
+      <div id="navDiv"></div>
+      <div id="profileDiv"></div>
+      <div id="headingDiv"></div>
+      <div id="headersDiv"></div>
+      <div id="mainDiv"></div>
+    </div>
+  );
+};
