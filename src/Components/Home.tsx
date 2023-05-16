a5960c1b12eede0df7abecde0587970cd793ee33
tanishaa17 <krati1701@gmail.com>
2023-05-17 03:49:50 +0530

    basic styling added

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,55 +1,127 @@
 export const Home = () => {
   return (
     <div className="home">
-      <div id="navDiv">
-        <div>
-          <img src="/assets/Images/logo.jpg" alt="logo" />
-          <img src="/assets/Images/right_symbol.jpg" alt="logo" />
+      <div
+        id="navDiv"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <img src="/assets/Images/logo.svg" alt="logo" />
+          <img src="/assets/Images/right_symbol.svg" alt="logo" />
         </div>
-        <div>
-          <img src="/assets/Images/search.jpg" alt="search" />
-          <img src="/assets/Images/mailbox.jpg" alt="mailbox" />
-          <img src="/assets/Images/notification.jpg" alt="notification" />
-          <img src="/assets/Images/question_circle.jpg" alt="question circle" />
-          <img src="/assets/Images/profile_pic.jpg" alt="profile picture" />
+        <div
+          style={{
+            display: "flex",
+            gap: "25px",
+            alignItems: "center",
+          }}
+        >
+          <img src="/assets/Images/search.svg" alt="search" />
+          <img src="/assets/Images/mailbox.svg" alt="mailbox" />
+          <img src="/assets/Images/notification.svg" alt="notification" />
+          <img src="/assets/Images/question_circle.svg" alt="question circle" />
+          <img src="/assets/Images/profile_pic.svg" alt="profile picture" />
         </div>
       </div>
-      <div id="headingBelowNav">
+      <div
+        id="headingBelowNav"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <h1>WeWork + SalesForce</h1>
         <button>Publish/Share</button>
       </div>
       <div id="profileDiv">
-        <img src="/assets/Images/img1.jpg" alt="question circle" />
-        <img src="/assets/Images/frame.jpg" alt="profile picture" />
+        <img
+          src="/assets/Images/img1.jpg"
+          alt="img1"
+          style={{
+            position: "absolute",
+          }}
+        />
+        <img
+          src="/assets/Images/frame.jpg"
+          alt="profile picture"
+          style={{
+            position: "absolute",
+            left: "101px",
+            top: "289px",
+            borderRadius: "10px",
+          }}
+        />
       </div>
-      <div id="headingDiv">
+      <div
+        id="headingDiv"
+        style={{
+          position: "absolute",
+          top: "400px",
+          color: "#233735",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
         <h3>Project Scope Recap</h3>
-        <img src="/assets/Images/action_plan.jpg" alt="" />
+        <img
+          src="/assets/Images/action_plan.svg"
+          alt="action plan"
+          style={{ position: "absolute", left: "1170px" }}
+        />
       </div>
-      <div id="headersDiv">
-        <div>
+      <div
+        id="headersDiv"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          position: "absolute",
+          top: "475px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
           <button>Basic Info</button>
           <button>Files</button>
           <button>Mutual Action Plan</button>
           <button>Contacts</button>
         </div>
-        <div>
+        <div style={{ position: "absolute", left: "1270px", width: "200px" }}>
           <button>Internal Actions</button>
         </div>
       </div>
-      <div id="mainDiv">
+      <div
+        id="mainDiv"
+        style={{
+          position: "absolute",
+          left: "31px",
+          top: "555px",
+          backgroundColor: "#FFFFFF",
+          borderRadius: "10px",
+          display: "flex",
+        }}
+      >
         <div
           style={{
-            // position: "absolute",
             width: "198px",
-            height: "290px",
-            left: "31px",
-            top: "555px",
-            backgroundColor: "#FFFFFF",
-            borderRadius: "10px",
           }}
         >
-          <ul style={{ listStyleType: "none" }}>
+          <ul style={{ listStyleType: "none", float: "left" }}>
             <h3>Overview</h3>
             <hr />
             <li>Welcome</li> <hr />
@@ -60,12 +132,42 @@ export const Home = () => {
           </ul>
         </div>
         <div>
-          <div>
-            <h3>Company Profile</h3>
-            <img src="/assets/Images/Group 1000003477.jpg" alt="" />
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <h3
+              style={{
+                position: "absolute",
+                left: "235px",
+                top: 0,
+                width: "150px",
+              }}
+            >
+              Company Profile
+            </h3>
+            <img
+              src="/assets/Images/Group 1000003477.svg"
+              alt=""
+              style={{
+                position: "absolute",
+                left: "1290px",
+                top: 0,
+              }}
+            />
           </div>
-          <img src="/assets/Images/img2.jpg" alt="" />
-          <img src="/assets/Images/msg_share.jpg" alt="" />
+          <div
+            style={{
+              position: "absolute",
+              left: "230px",
+              top: "70px",
+            }}
+          >
+            <img src="/assets/Images/img2.jpg" alt="" />
+            <img src="/assets/Images/msg_share.jpg" alt="" />
+          </div>
         </div>
       </div>
     </div>
