ccab7a6f642b0fbc5666d3f9e7d1b694fad83b8f
tanishaa17 <krati1701@gmail.com>
2023-05-17 02:05:49 +0530

    basic structure created

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,19 +1,73 @@
-import  '../Images/Ellipse 704.jpg'
 export const Home = () => {
   return (
     <div className="home">
-      <h1>Home page</h1>
       <div id="navDiv">
-        <img
-          src="../Images/Ellipse 704.svg"
-          alt=""
-          style={{ border: "5px solid black", width: "100px" }}
-        />
+        <div>
+          <img src="/assets/Images/logo.jpg" alt="logo" />
+          <img src="/assets/Images/right_symbol.jpg" alt="logo" />
+        </div>
+        <div>
+          <img src="/assets/Images/search.jpg" alt="search" />
+          <img src="/assets/Images/mailbox.jpg" alt="mailbox" />
+          <img src="/assets/Images/notification.jpg" alt="notification" />
+          <img src="/assets/Images/question_circle.jpg" alt="question circle" />
+          <img src="/assets/Images/profile_pic.jpg" alt="profile picture" />
+        </div>
       </div>
-      <div id="profileDiv"></div>
-      <div id="headingDiv"></div>
-      <div id="headersDiv"></div>
-      <div id="mainDiv"></div>
+      <div id="headingBelowNav">
+        <h1>WeWork + SalesForce</h1>
+        <button>Publish/Share</button>
+      </div>
+      <div id="profileDiv">
+        <img src="/assets/Images/img1.jpg" alt="question circle" />
+        <img src="/assets/Images/frame.jpg" alt="profile picture" />
+      </div>
+      <div id="headingDiv">
+        <h3>Project Scope Recap</h3>
+        <img src="/assets/Images/action_plan.jpg" alt="" />
+      </div>
+      <div id="headersDiv">
+        <div>
+          <button>Basic Info</button>
+          <button>Files</button>
+          <button>Mutual Action Plan</button>
+          <button>Contacts</button>
+        </div>
+        <div>
+          <button>Internal Actions</button>
+        </div>
+      </div>
+      <div id="mainDiv">
+        <div
+          style={{
+            // position: "absolute",
+            width: "198px",
+            height: "290px",
+            left: "31px",
+            top: "555px",
+            backgroundColor: "#FFFFFF",
+            borderRadius: "10px",
+          }}
+        >
+          <ul style={{ listStyleType: "none" }}>
+            <h3>Overview</h3>
+            <hr />
+            <li>Welcome</li> <hr />
+            <li>Product Capabilities</li> <hr />
+            <li>Customer Success Stories</li> <hr />
+            <li>File Sharing</li> <hr />
+            <li>Our Deck</li>
+          </ul>
+        </div>
+        <div>
+          <div>
+            <h3>Company Profile</h3>
+            <img src="/assets/Images/Group 1000003477.jpg" alt="" />
+          </div>
+          <img src="/assets/Images/img2.jpg" alt="" />
+          <img src="/assets/Images/msg_share.jpg" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
