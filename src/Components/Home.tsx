56107f9180989fc03a893e8b6310fdc5ca637dd8
tanishaa17 <krati1701@gmail.com>
2023-05-18 23:33:07 +0530

    added scss

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,31 +1,16 @@
+import '../SCSS/main.css'
+
+
 export const Home = () => {
   return (
     <div className="home">
       <div
-        id="navDiv"
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
+        className="navDiv">
+        <div className="navDivLeft">
           <img src="/assets/Images/logo.svg" alt="logo" />
           <img src="/assets/Images/right_symbol.svg" alt="logo" />
         </div>
-        <div
-          style={{
-            display: "flex",
-            gap: "25px",
-            alignItems: "center",
-          }}
-        >
+        <div className="navDivRight">
           <img src="/assets/Images/search.svg" alt="search" />
           <img src="/assets/Images/mailbox.svg" alt="mailbox" />
           <img src="/assets/Images/notification.svg" alt="notification" />
@@ -34,98 +19,42 @@ export const Home = () => {
         </div>
       </div>
       <div
-        id="headingBelowNav"
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
+        className="headingBelowNav">
         <h1>WeWork + SalesForce</h1>
-        <button style={{ backgroundColor: "#00B686", border: "none", color: "#FFFFFF", width: "147px", height: "40px", borderRadius: "5px" }}>Publish/Share</button>
+        <button>Publish/Share</button>
       </div>
-      <div id="profileDiv">
+      <div className="profileDiv">
         <img
-          src="/assets/Images/img1.jpg"
-          alt="img1"
-          style={{
-            position: "absolute",
-          }}
-        />
+          src="/assets/Images/img1.svg"
+          alt="img1" />
         <img
           src="/assets/Images/frame.jpg"
-          alt="profile picture"
-          style={{
-            position: "absolute",
-            left: "101px",
-            top: "289px",
-            borderRadius: "10px",
-          }}
+          alt="frame"
         />
       </div>
       <div
-        id="headingDiv"
-        style={{
-          position: "absolute",
-          top: "400px",
-          color: "#233735",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
+        className="headingDiv">
         <h3>Project Scope Recap</h3>
         <img
           src="/assets/Images/action_plan.svg"
           alt="action plan"
-          style={{ position: "absolute", left: "1170px" }}
         />
       </div>
       <div
-        id="headersDiv"
-        style={{
-          display: "flex",
-          alignItems: "center",
-          position: "absolute",
-          top: "475px",
-        }}
-      >
-        <div
+        className="headersDiv">
+        <div className="headersDivLeft"
           style={{
             display: "flex",
             alignItems: "center",
             gap: "8px",
           }}
         >
-          <button style={{
-            color: "#00B686",
-            border: "1px solid #BDC3C2",
-            fontWeight: "600",
-            height: "30px",
-            borderRadius: "3px"
-          }}>Basic Info</button>
-          <button style={{
-            color: "#657372",
-            border: "1px solid #BDC3C2",
-            fontWeight: "600",
-            height: "30px",
-            borderRadius: "3px"
-          }}>Files</button>
-          <button style={{
-            color: "#657372",
-            border: "1px solid #BDC3C2",
-            fontWeight: "600",
-            height: "30px",
-            borderRadius: "3px"
-          }}>Mutual Action Plan</button>
-          <button style={{
-            color: "#657372",
-            border: "1px solid #BDC3C2",
-            fontWeight: "600",
-            height: "30px",
-            borderRadius: "3px"
-          }}>Contacts</button>
+          <button>Basic Info</button>
+          <button >Files</button>
+          <button >Mutual Action Plan</button>
+          <button >Contacts</button>
         </div>
-        <div style={{ position: "absolute", left: "1270px", width: "200px" }}>
+        <div className="headersDivRight" style={{ position: "absolute", left: "1270px", width: "200px" }}>
           <button style={{
             color: "#657372",
             border: "1px solid #BDC3C2",
@@ -135,7 +64,7 @@ export const Home = () => {
         </div>
       </div>
       <div
-        id="mainDiv"
+        className="mainDiv"
         style={{
           position: "absolute",
           left: "31px",
@@ -196,7 +125,7 @@ export const Home = () => {
               top: "70px",
             }}
           >
-            <img src="/assets/Images/img2.jpg" alt="img2" width="1195px" />
+            <img src="/assets/Images/img2.svg" alt="img2" width="1195px" />
             <img src="/assets/Images/msg_share.svg" alt="" />
           </div>
         </div>
